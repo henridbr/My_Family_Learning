be40db0e40b87cfc65f433c8e0ed10d17bed031c
henridbr <henridbr@users.noreply.github.com>
2018-10-10 15:28:00 +0200

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -52,7 +52,7 @@ class FamilyLearningSkill(MycroftSkill):
     @intent_handler(IntentBuilder("GrandDaughterIntent").require("GrandDaughterKeyword"))
     def handle_grand_daughter_intent(self, message):
 
-        with open("family.book.json", "r") as read_file:
+        with open("familybook.json", "r") as read_file:
             family = json.load(read_file)
 
         #print(family)
