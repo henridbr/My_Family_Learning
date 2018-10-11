2964cbc6c06e3f3a1c2100744eff3ad6f40d5241
henridbr <henridbr@users.noreply.github.com>
2018-10-11 09:21:36 +0200

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -170,18 +170,18 @@ class FamilyLearningSkill(MycroftSkill):
 
             
 #### Living Place
-    @intent_handler(IntentBuilder("LivingPlaceIntent").require("LivingPlaceKeyword").require("FamilyFirstName"))
+    @intent_handler(IntentBuilder("LivingPlaceIntent").require("LivingPlaceKeyword").optionally("FamilyFirstName"))
 #    @intent_handler(IntentBuilder("LivingPlaceIntent").require("LivingPlaceKeyword"))
     def handle_living_place(self, message):
-        
+#       member = "Elodie"  
+        member = message.data.get('FamilyFirstName')
+        print( member)
+               
         with open("./opt/mycroft/skills/skill_family_learning.henridbr/familybook.json", "r") as read_file:
             family = json.load(read_file)
 
         membersname = family['family_dictionary']['members']
-#        member = "Elodie"  
-        member = message.data.get('FamilyFirstName')
-        print( member)
-        
+
         memberslivingplace ={}
 
         i=0
